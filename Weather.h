6d325c16ed7d1f00d5c0af23d3036b005292753c
Jayaraj1203 <117705445+Jayaraj1203@users.noreply.github.com>
2022-11-08 23:36:47 +0800

    Update Weather.h

--- Weather.h
+++ Weather.h
@@ -9,7 +9,7 @@ import java.util.Random;
     public float precipitation;
     
     // operation : must be tightly coupled to the data
-    void generate(int Min, int Max) {
+    public static void generate(int Min, int Max) {
         temperature = rand() / (float) RAND_MAX * (Max - Min) + Min;
         wind = rand() / (float) RAND_MAX * Min;
         humidity = rand() / (float) RAND_MAX * 100;
