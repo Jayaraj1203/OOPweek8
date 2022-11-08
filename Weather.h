cc1cea855b4f5cd69fcaf793b3e2ab36a68d791b
Jayaraj1203 <117705445+Jayaraj1203@users.noreply.github.com>
2022-11-08 23:34:01 +0800

    Update Weather.h

--- Weather.h
+++ Weather.h
@@ -1,1 +1,18 @@
+import java.util.Random;
 
+ public class Weather { // a class consists of data + operation
+   
+    // data
+    public float temperature;
+    public float wind;
+    public float humidity;
+    public float precipitation;
+    
+    // operation : must be tightly coupled to the data
+    void generate(int Min, int Max) {
+        temperature = rand() / (float) RAND_MAX * (Max - Min) + Min;
+        wind = rand() / (float) RAND_MAX * Min;
+        humidity = rand() / (float) RAND_MAX * 100;
+        precipitation = rand() / (float) RAND_MAX * Min;
+    }
+} ;
